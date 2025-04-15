7c5d377af74a9d8bd5651cc0b2c6fddfd43667b2
Evan Eissler <evaneissler@icloud.com>
2025-04-15 18:30:14 -0500

    Nested routes and dynamic route matching

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,36 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router'
 
-// Import components to route to
+// Import the components that we want to route to
 import Home from '@/views/Home.vue'
-import BlogPosts from "@/views/BlogPosts.vue";
-import About from "@/views/About.vue";
+import BlogPosts from '@/views/BlogPosts.vue'
+import About from '@/views/About.vue'
+import BlogPost from '@/views/BlogPost.vue'
+import BlogPostsGreeting from '@/views/BlogPostsGreeting.vue'
+import NotFound from '@/views/NotFound.vue'
 
 // Create a router instance
 const router = createRouter({
-    // Provide history implementation to use
-    history: createWebHistory(),
-    // Define routes, each route maps to a component
-    routes: [
-        { path: '/', name: 'home',component: Home },
-        { path: '/blogPosts', name: 'blogPosts', component: BlogPosts },
-        { path: '/about', name: 'about', component: About }
-    ]
+  history: createWebHistory(),
+  routes: [
+    { path: '/', name: 'home', component: Home },
+    {
+      path: '/blogPosts',
+      name: 'blogPosts',
+      component: BlogPosts,
+      redirect: { name: 'blogPostsGreeting' },
+      children: [
+        { path: '', name: 'blogPostsGreeting', component: BlogPostsGreeting },
+        { path: '/blogPosts/:id(\\d+)', name: 'blogPost', component: BlogPost },
+      ],
+    },
+    { path: '/about', name: 'about', component: About },
+    {
+      path: '/:pathMatch(.*)*', 
+      name: 'notFound',
+      component: NotFound,
+    },
+  ],
 })
 
-// Export router instance
+// Export the router instance
 export default router
